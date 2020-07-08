6a001c932452574cf8a845bb470e508a89365935
David Acosta <acosta816@gmail.com>
2020-07-08 16:49:46 -0700

    added .travis.yml

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React Trello with testing
 
 [GitHub](https://github.com/Thinkful-Ed/react-trello-testing/)
+
+# https://evening-falls-16772.herokuapp.com/
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,5 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()});
+
+//This file is run automatically before any tests. It sets up an adaptor, which lets Enzyme know how to work with components made with a specific version of React.
